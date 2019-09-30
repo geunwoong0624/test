6a1ecabca500bd46366622b910b79fba751907ab
GeunwoongSim <geuwnoong.sim@gmail.com>
2019-09-30 20:51:53 +0900

    window first commit

---
+++ Project1/main.cpp
@@ -0,0 +1,16 @@
+//
+// main.cpp
+// project
+//
+// Created by (name) on 20xx. x. x..
+// Copyright 20xx³â (nickname). All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	return 0;
+}
